a48802448a419bcfa0fe4982c31bbd65ff5962f0
abuzar08 <ak.abuzar@gmail.com>
2022-04-30 11:17:52 -0400

    Stats + BCNN - untested

---
+++ models/bcnn.py
@@ -0,0 +1,94 @@
+from turtle import forward
+import torch
+from torch import nn
+
+def enable_dropout(model):
+    '''
+    DESC
+    ---
+    This function sets the dropout layers to train mode
+    ---
+    INPUTS
+    ---
+
+    model: the model
+    ---
+    OUTPUTS
+    ---
+    None
+
+    '''
+    for m in model.modules():
+        if m.__class__.__name__.startswith('Dropout'):
+            m.train()
+
+class bcnn_block(nn.Module):
+    '''
+    DESC
+    ---
+    A conv-act-dropout-pool block with MC dropout
+    ---
+    INPUTS
+    ---
+    in_channels: input channels
+    out_channels: output_channels
+    kernel_size: kernel size
+    p: dropout rate
+
+    '''
+    def __init__(self, in_channels, out_channels, kernel_size, p):
+        super().__init__()
+        self.conv = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
+        self.relu = nn.ReLU()
+        self.Dropout = nn.Dropout(p)
+        self.pool = nn.MaxPool1d(2)
+    
+    def forward(self, x, final = False):
+        out = self.conv(x)
+        out = self.relu(out)
+        out = self.Dropout(out)
+        if not final:
+            out = self.pool(out)
+
+        return out
+
+class Bayes_CNN(nn.Module):
+    '''
+    DESC
+    ---
+    A Bayes CNN module
+    ---
+    INPUTS
+    ---
+    in_channels: input channels
+    out_channels_list: list of output_channels
+    kernel_sizees: list of kernel sizes
+    dropout_probs: lits of dropout rates
+
+    '''
+    def __init__(self, in_channels, out_channel_list, dropout_probs, kernel_sizes):
+        super().__init__()
+        in_channel_list = [in_channels] + out_channel_list[:-1]
+        self.blocks = nn.ModuleList([
+            bcnn_block(in_channel_list[i], out_channel_list[i], kernel_sizes[i], dropout_probs[i])
+            for i in len(out_channel_list)
+        ])
+        self.final_pool = nn.AdaptiveMaxPool1d(1)
+
+        self.linear= nn.Linear(128,2)
+    
+    def forward(self, x):
+
+        out = x
+        num_blocks = len(self.blocks)
+        for i,module in enumerate(self.blocks):
+            if i < num_blocks-1:
+                out = module(out)   
+            else:
+                out = module(out, final = True)     
+        
+        out = self.final_pool(out).squeeze()
+
+        out = self.linear(out)
+
+        return out
--- utils/statistics.py
+++ utils/statistics.py
@@ -1,9 +1,29 @@
-from audioop import cross
-from email.mime import base
+import json
 from scipy.stats import wilcoxon
 from scipy.stats import friedmanchisquare as friedman
 
 def report_wilcoxon(baseline: list, scores: list, names: list)->tuple:
+    '''
+    DESC
+    ---
+    This function creates wilcoxon test results for the baseline scores 
+    with all implemented scores, and amongst all implemented scores
+    ---
+    INPUTS
+    ---
+
+    baseline: list of scores for the baseline implementation
+    scores: list of lists pf scores of implementations
+    names: list of strings with names of implementations
+    ---
+    OUTPUTS
+    ---
+    baseline_stats: (dict) dictionary of type 
+                    {implementation_name : wilcoxon result with baseline}
+    cross_stats: (dict) dictionary of type 
+                {implementation_1_name : {implementation_2_name : wilcoxon result with implementation_1}
+
+    '''
     baseline_stats = {}
     for name, score in zip(names, scores):
         baseline_stats[name] = wilcoxon(score, baseline)
@@ -17,5 +37,58 @@ def report_wilcoxon(baseline: list, scores: list, names: list)->tuple:
     return baseline_stats, cross_stats
 
 def report_friedman(baseline, scores):
+    '''
+    DESC
+    ---
+    This function creates friedmann_chi_squared test results for the baseline scores 
+    with all implemented scores
+    ---
+    INPUTS
+    ---
+
+    baseline: list of scores for the baseline implementation
+    scores: list of lists pf scores of implementations
+    ---
+    OUTPUTS
+    ---
+    freidmann stats: scipy.stats object with stat and p value
+
+    '''
     all_scores = [baseline] + scores
-    return friedman(all_scores)
+    return friedman(*all_scores)
+
+
+def get_statistics(metric: str, baseline: list, scores: list, names: list, file_path = None):
+    '''
+    DESC
+    ---
+    This function creates friedmann_chi_squared test results for the baseline scores 
+    with all implemented scores
+    ---
+    INPUTS
+    ---
+
+    baseline: list of scores for the baseline implementation
+    scores: list of lists pf scores of implementations
+    names: list of strings with names of implementations
+    file_path: (str, None) path to location where statistics need to be saved
+    ---
+    OUTPUTS
+    ---
+    stat: dictionary with all statistical test stats
+
+    '''
+    baseline_wilcoxon_stats, cross_wilcoxon_stats = report_wilcoxon(baseline, scores, names)
+    friedman_stats = report_friedman(baseline,scores)
+    stat = {}
+    stat["wilcoxon_baseline"] = baseline_wilcoxon_stats
+    stat["wilcoxon_cross"] = cross_wilcoxon_stats
+    stat["friedmann"] = friedman_stats
+    
+    try:
+        with open(file_path, "w") as f:
+            json.dump(stat, f, indent=2)
+    except:
+        pass
+    
+    return stat
